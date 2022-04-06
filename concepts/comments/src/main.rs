5cadc2a727ce0f744e245c1396a18b1d6ee0b6b8
Teague Hannam <teaguehannam@gmail.com>
2022-04-06 06:06:38 -0600

    3.4 Comments

---
+++ concepts/comments/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Can do a single line
+    /*
+        Multiple lines
+        I think... docs dont mention this style of commenting
+    */
+    println!("Hello, world!"); // Can also comment end at end of line
+}
