38768abdd70f3a2e6c1dbf25f5da3191615b1275
Teague Hannam <teaguehannam@gmail.com>
2022-04-07 02:33:26 -0600

    7.3 Referring to Item in Module Tree

--- project_management/my-project/restaurant/src/lib.rs
+++ project_management/my-project/restaurant/src/lib.rs
@@ -1,6 +1,7 @@
+
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -8,11 +9,11 @@ mod front_of_house {
     mod serving {
         fn take_order() {}
 
-        fn serve_order() {}
-
         fn take_payment() {}
     }
 }
+
+fn serve_order() {}
 
 /*
 crate
@@ -20,8 +21,51 @@ crate
      ├── hosting
      │   ├── add_to_waitlist
      │   └── seat_at_table
+     ├── serve_order (super example)
      └── serving
          ├── take_order
-         ├── serve_order
          └── take_payment
 */
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        // super goes to parent module of back_of_house (root)
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    // Can be used by eat_at_restaurant (pub)
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+
