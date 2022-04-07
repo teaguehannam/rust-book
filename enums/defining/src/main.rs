fcb287d591823af0ed5411d1342abe34374bacc4
Teague Hannam <teaguehannam@gmail.com>
2022-04-07 00:55:10 -0600

    6.1 Defining an Enum

---
+++ enums/defining/src/main.rs
@@ -0,0 +1,31 @@
+// This doesn't do anything,
+// list of definitions
+
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn some_function() {
+        println!("Message printing...")
+    }
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    let localhost = IpAddrKind::V4(127, 0, 0, 1);
+}
+
+fn route(ip_kind: IpAddrKind) {}
