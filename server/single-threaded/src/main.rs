1d16f1c7ef625d7c4a0996f3e1f713bcf86e5a1f
Teague Hannam <teaguehannam@gmail.com>
2022-04-15 06:35:57 -0600

    Deliver JSON

--- server/single-threaded/src/main.rs
+++ server/single-threaded/src/main.rs
@@ -2,31 +2,29 @@ use std::io::prelude::*; // read/write to stream
 use std::net::TcpListener; // listen for requests
 use std::net::TcpStream; // single stream format
 use std::fs; // get html file for response
+use std::str; // we are the parser now
 
-fn main() {
-    // .bind() returns Result<T, E>
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    // stream = single open client server conneciton attempt
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-    }
-}
-
-// Single open connection between client & server
+// Single open connection
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024]; // 1024 bytes on stack
-    stream.read(&mut buffer).unwrap(); // read bytes from stream
+    let mut buffer = [0; 32]; // stack 32 bytes
+    stream.read(&mut buffer).unwrap(); // write to buffer
 
     let get = b"GET / HTTP/1.1\r\n";
+    let nutrients = b"GET /nutrients HTTP/1.1\r\n";
+
+    println!("{:?}", str::from_utf8(&buffer));
+
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "index.html")
+        // root directory
+        ("HTTP/1.1 200 OK", "./public/index.html")
+    } else if buffer.starts_with(nutrients) {
+        // nutrients directory
+        ("HTTP/1.1 200 OK", "./public/nutrients.json")
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+        // no match
+        ("HTTP/1.1 404 NOT FOUND", "./public/404.html")
     };
-    println!("{}, {}", status_line, filename);
+    // println!("{}, {}", status_line, filename);
 
     // HTML file for response
     let contents = fs::read_to_string(filename).unwrap();
@@ -42,4 +40,15 @@ fn handle_connection(mut stream: TcpStream) {
     // send it
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap(); // end stream
+}
+
+fn main() {
+    // Result<T, E>
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    // single client server connection attempt
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
 }
