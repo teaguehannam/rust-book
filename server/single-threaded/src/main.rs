8653a0cc6dd2b97d0b06c24d3e59980ccd7d5afd
Teague Hannam <teaguehannam@gmail.com>
2022-04-15 07:47:28 -0600

    File structuring

--- server/single-threaded/src/main.rs
+++ server/single-threaded/src/main.rs
@@ -2,24 +2,23 @@ use std::io::prelude::*; // read/write to stream
 use std::net::TcpListener; // listen for requests
 use std::net::TcpStream; // single stream format
 use std::fs; // get html file for response
-use std::str; // we are the parser now
 
 // Single open connection
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 32]; // stack 32 bytes
-    stream.read(&mut buffer).unwrap(); // write to buffer
+    let mut buffer = [0; 64]; // 64 byte stack
+    stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
-    let nutrients = b"GET /nutrients HTTP/1.1\r\n";
-
-    println!("{:?}", str::from_utf8(&buffer));
+    let api_root = b"GET /api HTTP/1.1\r\n";
+    let nutrients = b"GET /api/nutrients HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         // root directory
-        ("HTTP/1.1 200 OK", "./public/index.html")
+        ("HTTP/1.1 200 OK", "./public/home.html")
+    } else if buffer.starts_with(api_root) {
+        ("HTTP/1.1 200 OK", "./public/api/index.html")
     } else if buffer.starts_with(nutrients) {
-        // nutrients directory
-        ("HTTP/1.1 200 OK", "./public/nutrients.json")
+        ("HTTP/1.1 200 OK", "./public/api/nutrients.json")
     } else {
         // no match
         ("HTTP/1.1 404 NOT FOUND", "./public/404.html")
